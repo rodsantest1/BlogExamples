6a99bef238ce171890aa56519f8c3b25b6756c25
Rodney Santiago <rodney@rcsan96.onmicrosoft.com>
2024-08-15 01:26:25 -0400

    Keep all counters in sync

---
+++ ModalDialogComponent/CounterState.cs
@@ -0,0 +1,16 @@
+﻿namespace ModalDialogComponent
+{
+    public class CounterState
+    {
+        public int Count { get; set; }
+        public event Action OnChange;
+
+        public void IncrementCount()
+        {
+            Count++;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
